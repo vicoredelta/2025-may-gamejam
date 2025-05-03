f03ef40118ab1c3ee18e4e26ee61cb58b994aec7
GillisGrondahl <79366161+GillisGrondahl@users.noreply.github.com>
2025-05-03 18:02:29 +0200

    Mapändringar
    Mapändringar

--- scripts/Map.cs
+++ scripts/Map.cs
@@ -3,24 +3,32 @@ using System;
 
 public partial class Map : Node
 {
+	private Node2D RoomHolder;
 	
+	public override void _Ready()
+	{
+		RoomHolder = GetNode<Node2D>("RoomsHolder");
+	}
+	
+	/*
 	public void setRoom (string currentRoom)
 	{
 		switch (currentRoom)
 		{
 			case x:
-				$RoomsHolder.Position =  new Vector2(0, 0);
+				RoomHolder.Position =  new Vector2(0, 0);
 				
 			break;	
 			case y:
-				$RoomsHolder.Position =  new Vector2(0, 54);
+				RoomHolder.Position =  new Vector2(0, 54);
 			break;	
 			case z:
-				$RoomsHolder.Position =  new Vector2(54, 54);
+				RoomHolder.Position =  new Vector2(54, 54);
 			break;	
 		
 		}
 	}
+	*/
 	private void MoveMap ()
 	{
 		
