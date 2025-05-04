4b8c36bff3b10b1d7889fdf69089bc15ba4ec878
vicore <vicoredelta@proton.me>
2025-05-04 11:43:01 +0200

    Finish code for Inventory screen

--- scripts/InventoryScreen.cs
+++ scripts/InventoryScreen.cs
@@ -1,6 +1,25 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class InventoryScreen : ItemList
 {
+	public void ModifyInventory(Item item, bool itemIsBeingAdded)
+	{
+		// List to keep track of which order the items are listed in
+		// since Godot doesn't provide a method to access item by name
+		List<String> itemList = new List<String>();
+		
+		if (itemIsBeingAdded)
+		{
+			AddItem(item.Name, null, false);
+			itemList.Add(item.Name);
+		}
+		else
+		{
+			int index = itemList.IndexOf(item.Name);
+			itemList.RemoveAt(index);
+			RemoveItem(index);
+		}
+	}
 }
