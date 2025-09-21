d8415cd3be5556a6d64f4b5a3385671fd1d08b10
Emil Åberg <emilgamma@gmail.com>
2025-09-21 14:58:33 +0200

    add feedback when entering invalid look command

--- scripts/commands/LookCommand.cs
+++ scripts/commands/LookCommand.cs
@@ -15,17 +15,17 @@ public class LookCommand : Command
 		words = words.Skip(1).ToArray();
 		
 		List <ItemType> list = new List<ItemType>();
-		String[] remainderText = Parser.AddNextItem(words, list, Player.Instance.GetItemsInVicinity());
+		Parser.AddNextItem(words, list, Player.Instance.GetItemsInVicinity());
 		
 		if (list.Count > 0)
 		{
 			// Output description of item if an item was specified
 			return new CommandResult(this, list[0].Description);
 		}
-		else if (remainderText.Length > 0)
+		else if (words.Length > 0)
 		{
 			// Output error message if command was followed by text that does not include an item
-			return new CommandResult(this, remainderText[0] + " does not specify an " +
+			return new CommandResult(this, "'" + words[0] + "' does not specify an " +
 				"item in vicinity.");
 		}
 		else
